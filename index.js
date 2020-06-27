e28ab2d8bd313002f44c999447baa54bbd4b1027
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-06-27 00:00:22 -0700

    install Express, EJS, PG; Procfile for Heroku; directory skeleton; index.js skeleton

---
+++ index.js
@@ -0,0 +1,26 @@
+//Dependencies
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+//Can move database to another file, as needed
+const { Pool } = require('pg');
+
+//Imported from other folders
+
+
+let app = express();
+
+//utilities
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//paths
+//login path
+//Landing page path
+//etc.
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
