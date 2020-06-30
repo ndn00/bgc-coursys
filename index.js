37560078724cb6537e5759ca1e103085cd63be34
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-06-29 17:34:12 -0700

    install packages: express-session, passport, passport-local, bodyparser etc.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //Dependencies
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 //Can move database to another file, as needed
 const { Pool } = require('pg');
 
 //Imported from other folders
-
+const loginInfo = require('./loginInfo');
 
 let app = express();
 
@@ -25,7 +26,7 @@ app.set('view engine', 'ejs');
 app.get('/login', (req,res) => {
     // res.send("Hello World!");
     res.render('pages/login');
-})
-
+});
+app.post('/login', loginInfo.authenticate);
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
