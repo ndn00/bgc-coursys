75cb1b5352b24cfa74e91027b1f81a9e52bf4af2
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-07-07 12:14:57 -0700

    successfully hash password, insert email, hash, attendee (default) into database

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
-//const {requiresLogin, requiresOrganizer } = require('./middleware/auth');
+const {requiresLogin, requiresOrganizer } = require('./middleware/auth');
 //const organizer = require('../userTypes/organizer');
 const users = require('../userTypes/users');
 
@@ -9,8 +9,9 @@ module.exports = (app, passport, database) => {
   //app.get('/logout', users.logout);
 
   //create new user
-  app.get('/signup', users.signup);
-  //add new function for creating new user
+  app.get('/newuser', users.signup);
+  app.post('/newuser', users.createAccount);
+
 
 
   //add additional routes that require authentication to access, else they will be redirected to login
--- userTypes/users/index.js
+++ userTypes/users/index.js
@@ -1,3 +1,10 @@
+const database = require('../../database');
+
+//password storage setup
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 module.exports = {
   renderLogin: (request, result) => {
     console.log("called renderLogin");
@@ -23,6 +30,59 @@ module.exports = {
 
   signup: (request, result) => {
     result.render('pages/signUp');
+  },
+
+  createAccount: (request, result) => {
+    //do regular expression checks
+    let emailRegex = /[^(@|\s)]+@bgcengineering.ca/;
+    let mainPassRegex = /[\w|\~|\`|\!|\@|\#|\$|\%|\^|\*|\(|\)|\-|\{|\}|\[|\]|\'|\,|\.|\/|\\|:]{8,}/;
+    let uppercaseRegex = /[A-Z]/g;
+    let lowercaseRegex = /[a-z]/g;
+    let numberRegex = /[0-9]/g;
+
+
+    let email = request.body.uname.match(emailRegex);
+    let password = request.body.pwd.match(mainPassRegex);
+
+    //other password checks
+    let uppercase = request.body.pwd.match(uppercaseRegex);
+    let lowercase = request.body.pwd.match(lowercaseRegex);
+    let number = request.body.pwd.match(numberRegex);
+
+    if (email === null) {
+      return result.json("Invalid email, try again.");
+    }
+    if (password === null || uppercase === null || lowercase === null || number === null) {
+      return result.json("Invalid password, try again.");
+    }
+    //check to see if user is already in database
+    database.query('SELECT email FROM users WHERE email=$1;', email, (errOutDB, dbRes) => {
+      if (errOutDB) {
+        return result.json("Database error - looking up existing email");
+      }
+      if (dbRes.rows.length > 0) {
+        return result.json("Email already exists in database");
+      }
+      else {
+        //create new password
+        bcrypt.hash(password[0], saltRounds, (errHash, hash) => {
+          if (errHash) {
+            return result.json("Could not hash password properly");
+          }
+          console.log(email[0] + " " + hash);
+          database.query('INSERT INTO users (email, password, type) VALUES ($1, $2, $3);', [email[0], hash, "attendee"], (errInDB, dbRes2) => {
+            if (errInDB) {
+              return result.json("Database error - could not insert");
+            }
+            return result.json("Successfully inserted");
+          });
+        });
+      }
+    })
+
+
+
+
   }
 
 }
